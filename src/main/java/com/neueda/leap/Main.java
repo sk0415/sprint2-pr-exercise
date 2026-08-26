9e0e22212ec022785ab3b4113f8c78cd68820de2
Sanjana Kotha <sanjana1kotha@gmail.com>
2026-08-26 19:27:43 +0000

    testing change

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
         System.out.println("Container is up. Sleeping so you can inspect it.");
+        System.out.println("testing");
         Thread.sleep(600_000);
     }
 }
